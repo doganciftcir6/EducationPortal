e000e998a8f4f7dc6cdc6430aaa523e742443c35
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 19:34:35 +0300

    feat: AppRole ile ilgili repository sınıfları oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Repositories/Concrete/AppRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using EducationPortalApp.DataAccess.Contexts.EntityFramework;
+using EducationPortalApp.DataAccess.Repositories.GenericRepositories;
+using EducationPortalApp.DataAccess.Repositories.Interfaces;
+using EducationPortalApp.Entities.UserEntities;
+
+namespace EducationPortalApp.DataAccess.Repositories.Concrete
+{
+    public class AppRoleRepository : GenericRepository<AppRole>, IAppRoleRepository
+    {
+        public AppRoleRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+    }
+}
---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Repositories/Interfaces/IAppRoleRepository.cs
@@ -0,0 +1,9 @@
+﻿using EducationPortalApp.DataAccess.Repositories.GenericRepositories;
+using EducationPortalApp.Entities.UserEntities;
+
+namespace EducationPortalApp.DataAccess.Repositories.Interfaces
+{
+    public interface IAppRoleRepository : IGenericRepository<AppRole>
+    {
+    }
+}
