e265b6c9ceca2045da502b69562664be53635520
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 19:14:16 +0300

    feat: EnrollmentRequestStatus ile ilgili repository sınıfları oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Repositories/Concrete/EnrollmentRequestStatusRepository.cs
@@ -0,0 +1,14 @@
+﻿using EducationPortalApp.DataAccess.Contexts.EntityFramework;
+using EducationPortalApp.DataAccess.Repositories.GenericRepositories;
+using EducationPortalApp.DataAccess.Repositories.Interfaces;
+using EducationPortalApp.Entities.EnrollmentEntities;
+
+namespace EducationPortalApp.DataAccess.Repositories.Concrete
+{
+    public class EnrollmentRequestStatusRepository : GenericRepository<EnrollmentRequestStatus>, IEnrollmentRequestStatusRepository
+    {
+        public EnrollmentRequestStatusRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+    }
+}
