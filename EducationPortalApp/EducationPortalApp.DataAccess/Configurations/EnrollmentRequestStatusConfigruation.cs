0e4b1f3bb7814f2ad727a02b91b88451209f9588
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 16:30:44 +0300

    feat: EnrollmentRequestStatusConfigruation sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Configurations/EnrollmentRequestStatusConfigruation.cs
@@ -0,0 +1,27 @@
+﻿using EducationPortalApp.Entities.EnrollmentEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EducationPortalApp.DataAccess.Configurations
+{
+    public class EnrollmentRequestStatusConfigruation : IEntityTypeConfiguration<EnrollmentRequestStatus>
+    {
+        public void Configure(EntityTypeBuilder<EnrollmentRequestStatus> builder)
+        {
+            builder.HasData(new EnrollmentRequestStatus[]
+          {
+                new()
+                {
+                    Definition = "Participation",
+                    Id = 1,
+                },
+                new()
+                {
+                    Definition = "Cancellation",
+                    Id = 2,
+                },
+          });
+            builder.Property(x => x.Definition).IsRequired().HasMaxLength(100);
+        }
+    }
+}
