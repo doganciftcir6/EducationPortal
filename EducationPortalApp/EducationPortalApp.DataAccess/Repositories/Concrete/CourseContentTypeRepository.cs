b4b80a7ecb37697239494e0c3584a90db10ab3d1
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 19:07:30 +0300

    feat: CourseContentType ile ilgili repository sınıfları oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Repositories/Concrete/CourseContentTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using EducationPortalApp.DataAccess.Contexts.EntityFramework;
+using EducationPortalApp.DataAccess.Repositories.GenericRepositories;
+using EducationPortalApp.DataAccess.Repositories.Interfaces;
+using EducationPortalApp.Entities.CourseEntities;
+
+namespace EducationPortalApp.DataAccess.Repositories.Concrete
+{
+    public class CourseContentTypeRepository : GenericRepository<CourseContentType>, ICourseContentTypeRepository
+    {
+        public CourseContentTypeRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+    }
+}
