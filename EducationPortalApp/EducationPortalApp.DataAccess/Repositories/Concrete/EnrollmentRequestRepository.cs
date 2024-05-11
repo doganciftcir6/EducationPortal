0bef7fd51395967eaa099abc85fe4469bed458af
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 19:11:41 +0300

    feat: EnrollmentRequest ile ilgili repository sınıfları oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Repositories/Concrete/EnrollmentRequestRepository.cs
@@ -0,0 +1,14 @@
+﻿using EducationPortalApp.DataAccess.Contexts.EntityFramework;
+using EducationPortalApp.DataAccess.Repositories.GenericRepositories;
+using EducationPortalApp.DataAccess.Repositories.Interfaces;
+using EducationPortalApp.Entities.EnrollmentEntities;
+
+namespace EducationPortalApp.DataAccess.Repositories.Concrete
+{
+    public class EnrollmentRequestRepository : GenericRepository<EnrollmentRequest>, IEnrollmentRequestRepository
+    {
+        public EnrollmentRequestRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+    }
+}
