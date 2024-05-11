002c741935832e3b6748f7a1ddc2ea6db6b6b593
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 20:19:32 +0300

    feat: AppUserDto sınıfı oluşturuldu.
    Kullanıcı verileri bu dto ile taşınacak.

---
+++ EducationPortalApp/EducationPortalApp.Dtos/AppUserDtos/AppUserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace EducationPortalApp.Dtos.AppUserDtos
+{
+    public class AppUserDto
+    {
+        public int Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Username { get; set; }
+        public string? Gender { get; set; }
+    }
+}
