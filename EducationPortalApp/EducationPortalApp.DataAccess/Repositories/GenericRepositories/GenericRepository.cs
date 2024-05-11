c2951db3f378f277187581144416b788106d3cc0
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 18:58:05 +0300

    feat: Generic repository pattern kullanıldı.

---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Repositories/GenericRepositories/GenericRepository.cs
@@ -0,0 +1,63 @@
+﻿using EducationPortalApp.DataAccess.Contexts.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EducationPortalApp.DataAccess.Repositories.GenericRepositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class, new()
+    {
+        protected readonly AppDbContext _appDbContext;
+        public GenericRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public virtual async Task<T> AsNoTrackingGetByFilterAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _appDbContext.Set<T>().Where(filter).AsNoTracking().SingleOrDefaultAsync(filter);
+        }
+
+        public virtual T Delete(T entity)
+        {
+            _appDbContext.Set<T>().Remove(entity);
+            return entity;
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _appDbContext.Set<T>().ToListAsync();
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllFilterAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _appDbContext.Set<T>().Where(filter).ToListAsync();
+        }
+
+        public virtual async Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _appDbContext.Set<T>().Where(filter).SingleOrDefaultAsync(filter);
+        }
+
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return await _appDbContext.Set<T>().FindAsync(id);
+        }
+
+        public IQueryable<T> GetQuery()
+        {
+            return _appDbContext.Set<T>().AsQueryable();
+        }
+
+        public virtual async Task<T> InsertAsync(T entity)
+        {
+            await _appDbContext.Set<T>().AddAsync(entity);
+            return entity;
+        }
+
+        public virtual T Update(T entity)
+        {
+            _appDbContext.Set<T>().Update(entity);
+            return entity;
+        }
+    }
+}
---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Repositories/GenericRepositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace EducationPortalApp.DataAccess.Repositories.GenericRepositories
+{
+    public interface IGenericRepository<T> where T : class, new()
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllFilterAsync(Expression<Func<T, bool>> filter);
+        Task<T> GetByIdAsync(int id);
+        Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter);
+        Task<T> AsNoTrackingGetByFilterAsync(Expression<Func<T, bool>> filter);
+        Task<T> InsertAsync(T entity);
+        T Update(T entity);
+        T Delete(T entity);
+        IQueryable<T> GetQuery();
+    }
+}
