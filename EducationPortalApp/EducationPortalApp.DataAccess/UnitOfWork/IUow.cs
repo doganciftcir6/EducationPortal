e9c50343a909315095a46a7b3d2713707f3c98f6
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 19:48:52 +0300

    feat: UnitOfWork Pattern kullanımı yapıldı.
    UnitOfWork Pattern ile business katmanı işlemlerinin tek bir kanal üzerinden yapılması sağlandı.

---
+++ EducationPortalApp/EducationPortalApp.DataAccess/UnitOfWork/IUow.cs
@@ -0,0 +1,10 @@
+﻿using EducationPortalApp.DataAccess.Repositories.GenericRepositories;
+
+namespace EducationPortalApp.DataAccess.UnitOfWork
+{
+    public interface IUow
+    {
+        IGenericRepository<T> GetRepository<T>() where T : class, new();
+        Task SaveChangesAsync();
+    }
+}
---
+++ EducationPortalApp/EducationPortalApp.DataAccess/UnitOfWork/Uow.cs
@@ -0,0 +1,24 @@
+﻿using EducationPortalApp.DataAccess.Contexts.EntityFramework;
+using EducationPortalApp.DataAccess.Repositories.GenericRepositories;
+
+namespace EducationPortalApp.DataAccess.UnitOfWork
+{
+    public class Uow : IUow
+    {
+        private readonly AppDbContext _appDbContext;
+        public Uow(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IGenericRepository<T> GetRepository<T>() where T : class, new()
+        {
+            return new GenericRepository<T>(_appDbContext);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
