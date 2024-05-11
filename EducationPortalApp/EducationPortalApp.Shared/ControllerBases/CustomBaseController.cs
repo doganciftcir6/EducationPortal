7d4206a80a4616b2d5f2a5d7e15dc08f1c2a5dba
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 20:10:08 +0300

    feat: CustomBaseController sınıfı oluşturuldu.
    Bu sınıf sayesinde endpointlerde business katmanından dönen verileri uzun uzun ele almaktan kurtulacağız.

---
+++ EducationPortalApp/EducationPortalApp.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,16 @@
+﻿using EducationPortalApp.Shared.Utilities.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationPortalApp.Shared.ControllerBases
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(CustomResponse<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode,
+            };
+        }
+    }
+}
