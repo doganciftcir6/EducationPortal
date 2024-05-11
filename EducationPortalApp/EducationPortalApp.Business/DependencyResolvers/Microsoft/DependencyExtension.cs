ffe41f1178748386b6df652c3acd963d40a2551b
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 20:47:09 +0300

    feat: DI Container içerisinde JWT konfigürasyonu yapıldı.

--- EducationPortalApp/EducationPortalApp.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
+++ EducationPortalApp/EducationPortalApp.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
@@ -1,8 +1,12 @@
 ﻿using EducationPortalApp.DataAccess.Contexts.EntityFramework;
 using EducationPortalApp.DataAccess.UnitOfWork;
+using EducationPortalApp.Shared.Utilities.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace EducationPortalApp.Business.DependencyResolvers.Microsoft
 {
@@ -18,6 +22,28 @@ namespace EducationPortalApp.Business.DependencyResolvers.Microsoft
 
             //Scopes,Singletons,Transients
             services.AddScoped<IUow, Uow>();
+
+            //JWT register (Auto şema)
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(opt =>
+            {
+                opt.RequireHttpsMetadata = false;
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = JwtTokenDefaults.ValidIssuer,
+                    ValidAudience = JwtTokenDefaults.ValidAudience,
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtTokenDefaults.Key)),
+                };
+            });
         }
     }
 }
