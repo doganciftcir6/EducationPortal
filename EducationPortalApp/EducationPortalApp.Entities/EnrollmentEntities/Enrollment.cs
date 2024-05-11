c04388d461acecf72214a8baf0fa1b8e23553d3c
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 14:01:04 +0300

    feat: Enrollment ile ilgili entity sınıfları oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Entities/EnrollmentEntities/Enrollment.cs
@@ -0,0 +1,19 @@
+﻿using EducationPortalApp.Entities.CourseEntities;
+using System.ComponentModel.DataAnnotations;
+
+namespace EducationPortalApp.Entities.EnrollmentEntities
+{
+    public class Enrollment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ EducationPortalApp/EducationPortalApp.Entities/EnrollmentEntities/EnrollmentRequest.cs
@@ -0,0 +1,18 @@
+﻿using EducationPortalApp.Entities.CourseEntities;
+using System.ComponentModel.DataAnnotations;
+
+namespace EducationPortalApp.Entities.EnrollmentEntities
+{
+    public class EnrollmentRequest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+        public string Status { get; set; } 
+    }
+}
