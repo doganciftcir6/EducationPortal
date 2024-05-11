25552cf5ac86cb94693105c13b33eaa85d0eb990
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 13:55:15 +0300

    feat: Category ile ilgili entity sınıfları oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EducationPortalApp.Entities
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+
+        public List<Category> Categories { get; set; }
+    }
+}
