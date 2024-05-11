582aaf3fdf62cd5eb8792275de2350e7c81e4ecb
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 19:32:46 +0300

    feat: AppUser ile ilgili repository sınıfları oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Repositories/Concrete/AppUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using EducationPortalApp.DataAccess.Contexts.EntityFramework;
+using EducationPortalApp.DataAccess.Repositories.GenericRepositories;
+using EducationPortalApp.DataAccess.Repositories.Interfaces;
+using EducationPortalApp.Entities.UserEntities;
+
+namespace EducationPortalApp.DataAccess.Repositories.Concrete
+{
+    public class AppUserRepository : GenericRepository<AppUser>, IAppUserRepository
+    {
+        public AppUserRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+    }
+}
---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Repositories/Interfaces/IAppUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using EducationPortalApp.DataAccess.Repositories.GenericRepositories;
+using EducationPortalApp.Entities.UserEntities;
+
+namespace EducationPortalApp.DataAccess.Repositories.Interfaces
+{
+    public interface IAppUserRepository : IGenericRepository<AppUser>
+    {
+    }
+}
