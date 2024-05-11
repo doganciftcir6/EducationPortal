870994783a64f2c73fe1ef58652e0c607d34b9e8
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 19:02:25 +0300

    feat: Course ile ilgili repository sınıfları oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Repositories/Concrete/CourseRepository.cs
@@ -0,0 +1,14 @@
+﻿using EducationPortalApp.DataAccess.Contexts.EntityFramework;
+using EducationPortalApp.DataAccess.Repositories.GenericRepositories;
+using EducationPortalApp.DataAccess.Repositories.Interfaces;
+using EducationPortalApp.Entities.CourseEntities;
+
+namespace EducationPortalApp.DataAccess.Repositories.Concrete
+{
+    public class CourseRepository : GenericRepository<Course>, ICourseRepository
+    {
+        public CourseRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+    }
+}
---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Repositories/Interfaces/ICourseRepository.cs
@@ -0,0 +1,9 @@
+﻿using EducationPortalApp.DataAccess.Repositories.GenericRepositories;
+using EducationPortalApp.Entities.CourseEntities;
+
+namespace EducationPortalApp.DataAccess.Repositories.Interfaces
+{
+    public interface ICourseRepository : IGenericRepository<Course>
+    {
+    }
+}
