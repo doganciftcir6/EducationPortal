02ef122f3188f2ffc76d1b900e80073218dec36a
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 13:40:39 +0300

    feat: Projenin katmanları oluşturuldu.
    Uygulamada katmanlı mimari kullanıldı.

---
+++ EducationPortalApp/Frontends/EducationPortalApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EducationPortalApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
