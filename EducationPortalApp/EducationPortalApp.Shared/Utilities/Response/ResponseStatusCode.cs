2d0ff60665b95d4780685541985607d2bd190dd3
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 20:02:36 +0300

    feat: ResponseStatusCode sınıfı oluşturuldu.
    Bu sınıf sayesinde response olarak geriye başarı veya başarısızlık gibi durumları işaret eden status code'ları döneceğiz.

---
+++ EducationPortalApp/EducationPortalApp.Shared/Utilities/Response/ResponseStatusCode.cs
@@ -0,0 +1,15 @@
+﻿namespace EducationPortalApp.Shared.Utilities.Response
+{
+    public class ResponseStatusCode
+    {
+        public const int OK = 200;
+        public const int CREATED = 201;
+        public const int NOT_FOUND = 404;
+        public const int BAD_REQUEST = 400;
+        public const int INTERNAL_SERVER_ERROR = 500;
+        public const int NO_CONTENT = 204;
+        public const int UNAUTHORİZED = 401;
+        public const int FORBİDDEN = 403;
+    }
+}
+}
