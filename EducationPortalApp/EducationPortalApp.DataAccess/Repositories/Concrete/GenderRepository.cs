7a439f573e1dcdeb3f7d81f6388f15dcea811cc7
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-11 19:36:20 +0300

    feat: Gender ile ilgili repository sınıfları oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Repositories/Concrete/GenderRepository.cs
@@ -0,0 +1,14 @@
+﻿using EducationPortalApp.DataAccess.Contexts.EntityFramework;
+using EducationPortalApp.DataAccess.Repositories.GenericRepositories;
+using EducationPortalApp.DataAccess.Repositories.Interfaces;
+using EducationPortalApp.Entities.UserEntities;
+
+namespace EducationPortalApp.DataAccess.Repositories.Concrete
+{
+    public class GenderRepository : GenericRepository<Gender>, IGenderRepository
+    {
+        public GenderRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+    }
+}
---
+++ EducationPortalApp/EducationPortalApp.DataAccess/Repositories/Interfaces/IGenderRepository.cs
@@ -0,0 +1,9 @@
+﻿using EducationPortalApp.DataAccess.Repositories.GenericRepositories;
+using EducationPortalApp.Entities.UserEntities;
+
+namespace EducationPortalApp.DataAccess.Repositories.Interfaces
+{
+    public interface IGenderRepository : IGenericRepository<Gender>
+    {
+    }
+}
