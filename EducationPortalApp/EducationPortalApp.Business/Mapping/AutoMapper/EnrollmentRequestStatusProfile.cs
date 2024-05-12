2a109f41ddfa894e02b452649b51e890e42f8908
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-12 17:15:22 +0300

    feat: EnrollmentRequestStatusProfile sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Business/Mapping/AutoMapper/EnrollmentRequestStatusProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EducationPortalApp.Dtos.EnrollmentRequestStatusDtos;
+using EducationPortalApp.Entities.EnrollmentEntities;
+
+namespace EducationPortalApp.Business.Mapping.AutoMapper
+{
+    public class EnrollmentRequestStatusProfile : Profile
+    {
+        public EnrollmentRequestStatusProfile()
+        {
+            CreateMap<EnrollmentRequestStatus, EnrollmentRequestStatusDto>().ReverseMap();
+        }
+    }
+}
