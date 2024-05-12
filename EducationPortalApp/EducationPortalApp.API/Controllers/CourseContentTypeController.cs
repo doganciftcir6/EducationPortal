85c44d598c2ef0b999a429b12cea8ac4b0558888
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-12 17:04:06 +0300

    feat: CourseContentTypeController sınıfı oluşturuldu.
    Bu sayede tüm kurs içerik tiplerini listeleyen endpoint oluşturularak test edildi.

---
+++ EducationPortalApp/EducationPortalApp.API/Controllers/CourseContentTypeController.cs
@@ -0,0 +1,23 @@
+﻿using EducationPortalApp.Business.Services.Interfaces;
+using EducationPortalApp.Shared.ControllerBases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationPortalApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseContentTypeController : CustomBaseController
+    {
+        private readonly ICourseContentTypeService _courseContentTypeService;
+        public CourseContentTypeController(ICourseContentTypeService courseContentTypeService)
+        {
+            _courseContentTypeService = courseContentTypeService;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetCourseContentTypes()
+        {
+            return CreateActionResultInstance(await _courseContentTypeService.GetCourseContentTypesAsync());
+        }
+    }
+}
