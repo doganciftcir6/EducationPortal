e3ae54455f32300485ec96fedb8baa6ffa52bf61
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-12 16:33:59 +0300

    feat: GenderProfile sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Business/Mapping/AutoMapper/GenderProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EducationPortalApp.Dtos.GenderDtos;
+using EducationPortalApp.Entities.UserEntities;
+
+namespace EducationPortalApp.Business.Mapping.AutoMapper
+{
+    public class GenderProfile : Profile
+    {
+        public GenderProfile()
+        {
+            CreateMap<Gender, GenderDto>().ReverseMap();
+        }
+    }
+}
