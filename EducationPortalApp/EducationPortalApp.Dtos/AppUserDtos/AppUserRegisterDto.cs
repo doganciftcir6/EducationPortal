c5a5e5ad6b99e7de0405cb10ef5f001efae0035e
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-12 14:43:01 +0300

    feat: AppUserRegisterDto sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Dtos/AppUserDtos/AppUserRegisterDto.cs
@@ -0,0 +1,15 @@
+﻿namespace EducationPortalApp.Dtos.AppUserDtos
+{
+    public class AppUserRegisterDto
+    {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+
+        public int GenderId { get; set; }
+    }
+}
