b18c614bc15aeaf16fa0c65951af65a9e0bf927c
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-12 14:46:58 +0300

    feat: RoleAssingSendDto sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Dtos/AppUserDtos/RoleAssingSendDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EducationPortalApp.Dtos.AppUserDtos
+{
+    public class RoleAssingSendDto
+    {
+        public string? RoleName { get; set; }
+        public int UserId { get; set; }
+    }
+}
