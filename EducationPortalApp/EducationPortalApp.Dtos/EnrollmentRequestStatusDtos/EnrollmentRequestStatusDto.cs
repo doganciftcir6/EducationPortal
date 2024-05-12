f78ba6b5b2372854e9d1991890488e7a13e75721
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-12 17:11:24 +0300

    feat: EnrollmentRequestStatusDto sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Dtos/EnrollmentRequestStatusDtos/EnrollmentRequestStatusDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EducationPortalApp.Dtos.EnrollmentRequestStatusDtos
+{
+    public class EnrollmentRequestStatusDto
+    {
+        public int Id { get; set; }
+        public string? Definition { get; set; }
+    }
+}
