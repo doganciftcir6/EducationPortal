03d8baeb940b209f02826fb01f95d7335c8d84d9
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-12 14:51:15 +0300

    feat: AppUserProfile sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Business/Mapping/AutoMapper/AppUserProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EducationPortalApp.Dtos.AppUserDtos;
+using EducationPortalApp.Entities.UserEntities;
+
+namespace EducationPortalApp.Business.Mapping.AutoMapper
+{
+    public class AppUserProfile : Profile
+    {
+        public AppUserProfile()
+        {
+            CreateMap<AppUser, AppUserRegisterDto>().ReverseMap();
+            CreateMap<AppUser, AppUserLoginDto>().ReverseMap();
+            CreateMap<AppUser, AppUserDto>().ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.Gender != null ? src.Gender.Definition : null)).ReverseMap();
+        }
+    }
+}
