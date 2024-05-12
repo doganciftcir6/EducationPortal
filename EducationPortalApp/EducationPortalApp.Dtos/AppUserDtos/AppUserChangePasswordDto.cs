4553e6ee4709c3bd2681f52dce4361b0919f6766
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-12 14:46:01 +0300

    feat: AppUserChangePasswordDto sınıfı hazırlandı.

---
+++ EducationPortalApp/EducationPortalApp.Dtos/AppUserDtos/AppUserChangePasswordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EducationPortalApp.Dtos.AppUserDtos
+{
+    public class AppUserChangePasswordDto
+    {
+        public string? CurrentPassword { get; set; }
+        public string? NewPassword { get; set; }
+        public string? ConfirmPassword { get; set; }
+    }
+}
