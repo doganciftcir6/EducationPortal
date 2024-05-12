1c82c5071a09cf642aaa284788b9283c8c438004
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-12 16:43:36 +0300

    feat: GenderController sınıfı oluşturuldu.
    Bu sayede tüm cinsiyetleri listeleyen endpoint oluşturularak test edildi.

---
+++ EducationPortalApp/EducationPortalApp.API/Controllers/GenderController.cs
@@ -0,0 +1,23 @@
+﻿using EducationPortalApp.Business.Services.Interfaces;
+using EducationPortalApp.Shared.ControllerBases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationPortalApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenderController : CustomBaseController
+    {
+        private readonly IGenderService _genderService;
+        public GenderController(IGenderService genderService)
+        {
+            _genderService = genderService;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetGenders()
+        {
+            return CreateActionResultInstance(await _genderService.GetGendersAsync());
+        }
+    }
+}
