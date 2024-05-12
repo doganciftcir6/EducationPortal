2cdc8913fcbe5b2bfc9cce98c9bcc3868f3ec1f1
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-12 16:45:14 +0300

    feat: CourseContentTypeDto sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Dtos/CourseContentTypeDtos/CourseContentTypeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EducationPortalApp.Dtos.CourseContentTypeDtos
+{
+    public class CourseContentTypeDto
+    {
+        public int Id { get; set; }
+        public string? Definition { get; set; }
+    }
+}
