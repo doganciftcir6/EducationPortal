0b7e27fad06123935fe6692f548e0847dc152e8a
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-12 15:04:15 +0300

    feat: AppUserMessages sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Business/Helpers/Messages/AppUserMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace EducationPortalApp.Business.Helpers.Messages
+{
+    public class AppUserMessages
+    {
+        public const string NOT_FOUND_ROLE = "Role not found!";
+        public const string NOT_FOUND_USER = "User not found!";
+        public const string WRONG_CURRENT_PASSWORD = "Wrong current password!";
+        public const string LOGİN_FAİLED = "Email or password is incorrect!";
+        public const string SUCCESS_REGİSTER = "User has been successfully created!";
+    }
+}
