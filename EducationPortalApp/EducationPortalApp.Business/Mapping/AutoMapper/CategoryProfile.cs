f37fbe3432f72beec14577f10638ce0d0f448e15
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-13 15:52:24 +0300

    feat: CategoryProfile sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Business/Mapping/AutoMapper/CategoryProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EducationPortalApp.Dtos.CategoryDtos;
+using EducationPortalApp.Entities;
+
+namespace EducationPortalApp.Business.Mapping.AutoMapper
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryCreateDto>().ReverseMap();
+            CreateMap<Category, CategoryUpdateDto>().ReverseMap();
+        }
+    }
+}
