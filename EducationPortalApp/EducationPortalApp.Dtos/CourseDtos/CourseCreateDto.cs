fe8e1ec910b55820851f3cf6c37bd79d6a6f74f7
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-13 14:20:07 +0300

    feat: Course ile ilgili dto sınıfları oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Dtos/CourseDtos/CourseCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace EducationPortalApp.Dtos.CourseDtos
+{
+    public class CourseCreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Instructor { get; set; }
+        public int Capacity { get; set; }
+        public decimal CostPerDay { get; set; }
+        public int DurationInHours { get; set; }
+        public IFormFile? Picture { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
---
+++ EducationPortalApp/EducationPortalApp.Dtos/CourseDtos/CoursesDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EducationPortalApp.Dtos.CourseDtos
+{
+    public class CoursesDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal CostPerDay { get; set; }
+        public string? Picture { get; set; }
+    }
+}
