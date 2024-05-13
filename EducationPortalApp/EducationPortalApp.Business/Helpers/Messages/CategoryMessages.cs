335efbd145db639c7a0da83aa13774b5f01a0fea
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-13 15:57:16 +0300

    feat: CategoryMessages sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Business/Helpers/Messages/CategoryMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace EducationPortalApp.Business.Helpers.Messages
+{
+    public class CategoryMessages
+    {
+        public const string NOT_FOUND_CATEGORY = "Category not found!";
+    }
+}
