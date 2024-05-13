204c68e123194706a20af28921b64b026c86358a
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-13 14:53:04 +0300

    feat: Course picture alanı için upload helper sınıfları oluşturuldu.
    Kurs resmini servera upload etmeye ve resmi serverdan silmeyi sağlayacak helper metotlar oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Business/Helpers/UploadHelpers/Course/CoursePictureDeleteHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace EducationPortalApp.Business.Helpers.UploadHelpers.Course
+{
+    public class CoursePictureDeleteHelper
+    {
+        public static void Delete(IHostingEnvironment hostingEnvironment, string file)
+        {
+            try
+            {
+                int index = file.IndexOf("CourseImages/");
+                string relativeFileName = index != -1 ? file.Substring(index + "CourseImages/".Length) : file;
+                string path = Path.Combine(hostingEnvironment.WebRootPath, "CourseImages", relativeFileName);
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ EducationPortalApp/EducationPortalApp.Business/Helpers/UploadHelpers/Course/CoursePictureUploadHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace EducationPortalApp.Business.Helpers.UploadHelpers.Course
+{
+    public class CoursePictureUploadHelper
+    {
+        public static async Task<string> Run(IHostingEnvironment hostingEnvironment, IFormFile file, IConfiguration configuration, CancellationToken cancellationToken)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(file.FileName) + Guid.NewGuid().ToString("N") + Path.GetExtension(file.FileName);
+            string path = Path.Combine(hostingEnvironment.WebRootPath, "CourseImages", fileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream, cancellationToken);
+                stream.Close();
+            }
+            var apiUrl = configuration["ApiSettings:ApiUrl"];
+            var fileUrl = $"{apiUrl}/CourseImages/{fileName}";
+
+            return fileUrl;
+        }
+    }
+}
