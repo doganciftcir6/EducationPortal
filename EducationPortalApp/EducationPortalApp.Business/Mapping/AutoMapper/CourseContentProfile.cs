6efe81abbcde439385df33564cc5c0886eb87313
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-13 18:47:36 +0300

    feat: CourseContentProfile sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Business/Mapping/AutoMapper/CourseContentProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EducationPortalApp.Dtos.CourseContentDtos;
+using EducationPortalApp.Entities.CourseEntities;
+
+namespace EducationPortalApp.Business.Mapping.AutoMapper
+{
+    public class CourseContentProfile : Profile
+    {
+        public CourseContentProfile()
+        {
+            CreateMap<CourseContent, CourseContentDto>().ForMember(dest => dest.CourseContentType, opt => opt.MapFrom(src => src.CourseContentType != null ? src.CourseContentType.Definition : null)).ReverseMap();
+            CreateMap<CourseContent, CourseContentCreateDto>().ReverseMap();
+            CreateMap<CourseContent, CourseContentUpdateDto>().ReverseMap();
+        }
+    }
+}
