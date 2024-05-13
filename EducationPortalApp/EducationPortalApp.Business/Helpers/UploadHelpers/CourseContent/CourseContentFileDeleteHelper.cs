7422a74c7afb3ec3c843f5dd64c47b44b05e2573
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-13 18:57:57 +0300

    feat: CourseContent file alanı için upload helper sınıfları oluşturuldu.
    Kurs içerik dosyasını servera upload etmeye ve dosyayı serverdan silmeyi sağlayacak helper metotlar oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Business/Helpers/UploadHelpers/CourseContent/CourseContentFileDeleteHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace EducationPortalApp.Business.Helpers.UploadHelpers.CourseContent
+{
+    public class CourseContentFileDeleteHelper
+    {
+        public static void Delete(IHostingEnvironment hostingEnvironment, string file)
+        {
+            try
+            {
+                int index = file.IndexOf("CourseContentFiles/");
+                string relativeFileName = index != -1 ? file.Substring(index + "CourseContentFiles/".Length) : file;
+                string path = Path.Combine(hostingEnvironment.WebRootPath, "CourseContentFiles", relativeFileName);
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ EducationPortalApp/EducationPortalApp.Business/Helpers/UploadHelpers/CourseContent/CourseContentFileUploadHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace EducationPortalApp.Business.Helpers.UploadHelpers.CourseContent
+{
+    public class CourseContentFileUploadHelper
+    {
+        public static async Task<string> Run(IHostingEnvironment hostingEnvironment, IFormFile file, IConfiguration configuration, CancellationToken cancellationToken)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(file.FileName) + Guid.NewGuid().ToString("N") + Path.GetExtension(file.FileName);
+            string path = Path.Combine(hostingEnvironment.WebRootPath, "CourseContentFiles", fileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream, cancellationToken);
+                stream.Close();
+            }
+            var apiUrl = configuration["ApiSettings:ApiUrl"];
+            var fileUrl = $"{apiUrl}/CourseContentFiles/{fileName}";
+
+            return fileUrl;
+        }
+    }
+}
