70cef573c06e87b1d213a460765d875d899c7f76
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-13 20:46:20 +0300

    feat: EnrollmentRequestProfile sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Business/Mapping/AutoMapper/EnrollmentRequestProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EducationPortalApp.Dtos.EnrollmentRequestDtos;
+using EducationPortalApp.Entities.EnrollmentEntities;
+
+namespace EducationPortalApp.Business.Mapping.AutoMapper
+{
+    public class EnrollmentRequestProfile : Profile
+    {
+        public EnrollmentRequestProfile()
+        {
+            CreateMap<EnrollmentRequest, EnrollmentRequestDto>().ForMember(dest => dest.Course, opt => opt.MapFrom(src => src.Course != null ? src.Course.Name : null)).ForMember(dest => dest.EnrollmentRequestStatus, opt => opt.MapFrom(src => src.EnrollmentRequestStatus != null ? src.EnrollmentRequestStatus.Definition : null)).ForMember(dest => dest.AppUser, opt => opt.MapFrom(src => src.AppUser != null ? $"{src.AppUser.Firstname} {src.AppUser.Lastname}".Trim() : null)).ReverseMap();
+            CreateMap<EnrollmentRequest, EnrollmentRequestCreateDto>().ReverseMap();
+            CreateMap<EnrollmentRequest, EnrollmentRequestUpdateDto>().ReverseMap();
+        }
+    }
+}
