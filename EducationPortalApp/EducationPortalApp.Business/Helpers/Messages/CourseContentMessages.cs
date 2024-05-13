f4423dfa7d85e0ead8e8ac9245579441b3afc766
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-13 18:54:48 +0300

    feat: CourseContentMessages sınıfı oluşturuldu.

---
+++ EducationPortalApp/EducationPortalApp.Business/Helpers/Messages/CourseContentMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace EducationPortalApp.Business.Helpers.Messages
+{
+    public class CourseContentMessages
+    {
+        public const string NOT_FOUND_COURSE_CONTENT = "Course content not found!";
+    }
+}
