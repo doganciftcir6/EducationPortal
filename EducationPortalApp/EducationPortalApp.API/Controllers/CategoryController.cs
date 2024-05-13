461fe3516de547c93d93d6befa0ebd2faeb5e435
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-13 18:31:21 +0300

    feat: CategoryController sınıfı oluşturuldu.
    Bu sayede kategori ile ilgili CRUD işlemlerin endpointleri hazırlanmış ve test edilmiş oldu.

---
+++ EducationPortalApp/EducationPortalApp.API/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using EducationPortalApp.Business.Services.Interfaces;
+using EducationPortalApp.Dtos.CategoryDtos;
+using EducationPortalApp.Shared.ControllerBases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationPortalApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetCategories()
+        {
+            return CreateActionResultInstance(await _categoryService.GetCategoriesAsync());
+        }
+
+        [HttpGet("[action]/{categoryId}")]
+        public async Task<IActionResult> GetCategory(int categoryId)
+        {
+            return CreateActionResultInstance(await _categoryService.GetCategoryByIdAsync(categoryId));
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> InsertCategory(CategoryCreateDto categoryCreateDto)
+        {
+            return CreateActionResultInstance(await _categoryService.InsertCategoryAsync(categoryCreateDto));
+        }
+
+        [HttpPut("[action]")]
+        public async Task<IActionResult> UpdateCategory(CategoryUpdateDto categoryUpdateDto)
+        {
+            return CreateActionResultInstance(await _categoryService.UpdateCategoryAsync(categoryUpdateDto));
+        }
+
+        [HttpDelete("[action]/{categoryId}")]
+        public async Task<IActionResult> RemoveCategory(int categoryId)
+        {
+            return CreateActionResultInstance(await _categoryService.RemoveCategoryAsync(categoryId));
+        }
+    }
+}
