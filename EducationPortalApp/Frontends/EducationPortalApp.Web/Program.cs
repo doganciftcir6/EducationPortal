1a15f013df1ea18d2d3ff158577dd1c1585e60f7
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2024-05-15 16:05:14 +0300

    feat: Temel frontend konfigürasyonları yapıldı.

--- EducationPortalApp/Frontends/EducationPortalApp.Web/Program.cs
+++ EducationPortalApp/Frontends/EducationPortalApp.Web/Program.cs
@@ -1,7 +1,28 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//API Base URL
+var baseApiAddress = builder.Configuration.GetSection("BaseApiAddress").Get<string>();
+builder.Services.AddHttpClient("MyApiClient", opt =>
+{
+    opt.BaseAddress = new Uri(baseApiAddress);
+});
+
+//Auth - Cookie Based
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddCookie(JwtBearerDefaults.AuthenticationScheme, opt =>
+{
+    opt.LoginPath = "/Auth/Login";
+    opt.LogoutPath = "/Auth/Logout";
+    opt.AccessDeniedPath = "/Auth/AccessDenied";
+    opt.Cookie.SameSite = SameSiteMode.Strict;
+    opt.Cookie.HttpOnly = true;
+    opt.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    opt.Cookie.Name = "eShopOnWebAppJwtCookie";
+});
 
 var app = builder.Build();
 
@@ -14,6 +35,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
